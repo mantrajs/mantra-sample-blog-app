ff2fc3c89636d72e4ef1af58912d2b82c4048400
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-01-10 15:59:07 +0530

    Test clients

---
+++ client/components/layouts/tests/main.js
@@ -0,0 +1,23 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import MainLayout from '../main.jsx';
+import Navigations from '../../navigations/index.jsx';
+
+describe('components.layouts.main', () => {
+  it('should contain navigations', () => {
+    const el = shallow(<MainLayout />);
+    expect(el.contains(Navigations)).to.be.true;
+  });
+
+  it('should render childrens', () => {
+    const Comp = () => (<p>Hello</p>);
+    const el = shallow(
+      <Navigations>
+        <Comp />
+      </Navigations>
+    );
+
+    expect(el.contains(Comp)).to.be.true;
+  });
+});
