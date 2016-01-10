bf034181db0957e9b020ff151d6d3af93730692c
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-01-10 17:06:32 +0530

    Add some docs

--- client/components/newpost/index.jsx
+++ client/components/newpost/index.jsx
@@ -1,5 +1,7 @@
 // XXX: We need to find a way to use npm's react
 // Otherwise, we can't run tests properly.
+// To test this component, use React from npm directly
+// as shown below
 import {React} from 'meteor/react-runtime';
 // import React from 'react';
 
