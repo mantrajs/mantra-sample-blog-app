877ddeb3f8808e92b3f4db05b53c659d8896cb04
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-01-11 05:45:58 +0530

    Add just test cases for containers.newpost

---
+++ client/containers/tests/newpost.js
@@ -0,0 +1,23 @@
+// We need require React from NPM before working on these tests
+
+// import {describe, it} from 'mocha';
+// import {expect} from 'chai';
+// import {stub, spy} from 'sinon';
+// import {composer, depsMapper} from '../newpost';
+
+// describe('containers.newpost', () => {
+//   describe('composer', () => {
+//     it('should get SAVING_ERROR from local state');
+//     it('should get SAVING_NEW_POST from local state');
+//   });
+
+//   describe('depsMapper', () => {
+//     describe('actions', () => {
+//       it('should map posts.create');
+//       it('should map posts.clearErrors');
+//     });
+//     describe('context', () => {
+//       it('should map the whole context as a function');
+//     });
+//   });
+// });
