1ed6c43f98dd57c275d6846758980f44ac6713a2
xcv58 <chenyihonglove@gmail.com>
2016-02-07 11:34:24 -0500

    add commented tests for comments.containers.create_comment

---
+++ client/modules/comments/containers/tests/create_comment.js
@@ -0,0 +1,22 @@
+// We need require React from NPM before working on these tests
+
+// const { describe, it } = global;
+// import {expect} from 'chai';
+// import {stub, spy} from 'sinon';
+// import {composer, depsMapper} from '../create_comment';
+
+// describe('comments.containers.create_comment', () => {
+//   describe('composer', () => {
+//     it('should get CREATE_COMMENT_ERROR from local state');
+//   });
+
+//   describe('depsMapper', () => {
+//     describe('actions', () => {
+//       it('should map comments.create');
+//       it('should map comments.clearErrors');
+//     });
+//     describe('context', () => {
+//       it('should map the whole context as a function');
+//     });
+//   });
+// });
