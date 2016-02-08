77d08f6182b32b89c7b4343e42f19ded387255ab
discdiver <jeffmshale@gmail.com>
2016-02-08 11:29:25 -0500

    changes flow router to non-ssr version so app works with meteor 1.3-modules-beta.8

--- client/configs/context.js
+++ client/configs/context.js
@@ -1,6 +1,6 @@
 import * as Collections from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
-import {FlowRouter} from 'meteor/kadira:flow-router-ssr';
+import {FlowRouter} from 'meteor/kadira:flow-router';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Tracker} from 'meteor/tracker';
 
