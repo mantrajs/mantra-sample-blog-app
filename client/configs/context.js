dd6eb865ab500cd3ac5aeaa6e666ac3831616212
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-16 15:37:57 -0700

    Update to use Meteor 1.3 RC 1

--- client/configs/context.js
+++ client/configs/context.js
@@ -1,6 +1,6 @@
 import * as Collections from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
-import {FlowRouter} from 'meteor/kadira:flow-router';
+import {FlowRouter} from 'meteor/kadira:flow-router-ssr';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Tracker} from 'meteor/tracker';
 
